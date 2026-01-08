263f16b0d327a707c08ce2b96dd6660c9943b457
mschuhmacher <michielschuhmacher@gmail.com>
2026-01-08 12:02:08 +0100

    Rename app to Flash Forward

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.training_schedule_app"
+        applicationId = "com.studiofoam.flashforward"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
