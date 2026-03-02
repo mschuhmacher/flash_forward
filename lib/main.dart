f75e1a7f3756e06987c1220d029886cd871bcf5b
mschuhmacher <michielschuhmacher@gmail.com>
2026-03-02 21:00:01 +0100

    Refactor Sentry initialization for not logging when debugging and adjust sampling rates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flash_forward/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flash_forward/presentation/screens/loading_screen.dart';
 import 'package:flash_forward/services/supabase_config.dart';
@@ -22,22 +23,19 @@ void main() async {
 
   await SentryFlutter.init(
     (options) {
-      options.dsn = dotenv.env['SENTRY_DSN'];
+      options.dsn = kDebugMode ? '' : dotenv.env['SENTRY_DSN'];
+
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
       options.sendDefaultPii = false;
       options.enableLogs = true;
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-      // We recommend adjusting this value in production.
-      options.tracesSampleRate = 1.0;
-      // The sampling rate for profiling is relative to tracesSampleRate
-      // Setting to 1.0 will profile 100% of sampled transactions:
-      options.profilesSampleRate = 1.0;
+      options.tracesSampleRate = kDebugMode ? 1.0 : 0.2;
+      options.profilesSampleRate = kDebugMode ? 1.0 : 0.5;
       // Configure Session Replay
       options.replay.sessionSampleRate = 0.1;
       options.replay.onErrorSampleRate = 1.0;
     },
-    appRunner: () => runApp(SentryWidget(child: 
+    appRunner: () => runApp(SentryWidget(child:
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
